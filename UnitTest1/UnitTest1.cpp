f6c61b9c807e16c0a7f9cbe8517151c7c690ee20
Yejin Shin <fx0605@naver.com>
2021-11-23 22:34:00 +0900

    //변경

--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -1,6 +1,13 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 
+#include "..//TestOpenSource/Triangle.cpp"
+
+
+std::wstring ToString(struct Triangle t)
+{
+	return std::to_wstring(t.width) + std::wstring(L"*") + std::to_wstring(t.height);
+}
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTest1
@@ -9,8 +16,17 @@ namespace UnitTest1
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(TriangleArea)
 		{
+			struct Triangle t1, result, compare;
+			t1.width = 1;
+			t1.height = 4;
+
+			result = Triangle_area(t1);
+			compare.width = 2;
+			compare.height = 3;
+			Assert::AreEqual(result, compare);
+			
 		}
 	};
 }
